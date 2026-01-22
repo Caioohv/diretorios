78b56366745e265d31af05d63801a20dffff03b3
Caio Vieira <caioviier@gmail.com>
2026-01-22 18:06:11 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -54,10 +54,13 @@ class Folder extends Item {
   }
 
   public void removeDir(String name) throws Exception {
+    System.out.println("removendo name: "+name);
     for (Item item : this.childs) {
       if (item.getName().equals(name) && item.getType().equals("dir")) {
         if(((Folder)item).countChilds() == 0) {
+          System.out.println("item: "+item);
           this.childs.remove(item);
+          System.out.println("this.childs: "+this.childs);
           return;
         } else {
           throw new Exception("Erro: Diretório não está vazio");
@@ -83,6 +86,16 @@ class Folder extends Item {
       }
     }
     this.childs.add(file);
+  }
+
+  public void removeFile(String name) throws Exception {
+    for (Item item : this.childs) {
+      if (item.getName().equals(name) && item.getType().equals("file")) {
+        this.childs.remove(item);
+        return;
+      }
+    }
+    throw new Exception("Erro: Arquivo não existe");
   }
 
   public void addDir(Folder dir) throws Exception {
@@ -127,6 +140,12 @@ class File extends Item {
   public String getContent() {
     return this.content;
   }
+  public void setContent(String newcontent) {
+    this.content = newcontent;
+  }
+  public void appendContent(String morecontent) {
+    this.content += morecontent;
+  }
 }
 
 class State {
@@ -165,26 +184,26 @@ class State {
 
 public class Main {
 
-  Folder mkdir(Folder current, String name) throws Exception {
+  static Folder mkdir(Folder current, String name) throws Exception {
     Folder toAdd = new Folder(name);
     current.addDir(toAdd);
     return current;
   }
 
-  Folder rmdir(Folder current, String name) throws Exception {
+  static Folder rmdir(Folder current, String name) throws Exception {
     current.removeDir(name);
     return current;
   }
 
-  void tree(Folder current) {
+  static void tree(Folder current) {
     current.tree("");
   }
 
-  void rename(Item i, String newname) {
+  static void rename(Item i, String newname) {
     i.setName(newname);
   }
 
-  void ls(Folder Folder) {
+  static void ls(Folder Folder) {
     ArrayList<Item> lista = Folder.getChilds();
 
     ArrayList<Folder> Folders = new ArrayList<>();
@@ -207,11 +226,11 @@ public class Main {
     }
   }
 
-  void cat(File file){
+  static void cat(File file){
     System.out.println(file.getContent());
   }
 
-  File touch(String name, String content){
+  static File touch(String name, String content){
     File f = new File(name, content);
     return f;
   }
@@ -239,37 +258,65 @@ public class Main {
       try{
         switch(partes[0]){
           case "mkdir":
-            currentDir = new Main().mkdir(currentDir, partes[1]);
+            currentDir = mkdir(currentDir, partes[1]);
             break;
           case "rmdir":
-            currentDir = new Main().rmdir(currentDir, partes[1]);
+            currentDir = rmdir(currentDir, partes[1]);
             break;
           case "tree":
-            new Main().tree(currentDir);
-            break;
-          case "ls":
-            new Main().ls(currentDir);
-            break;
-          case "touch":
-            if(partes.length == 2) {
-              File f = new Main().touch(partes[1], "");
-              currentDir.addFile(f);
-              break;
-            }
-            File f = new Main().touch(partes[1], partes[2]);
-            currentDir.addFile(f);
-            break;
-          case "cat":
-            File file = currentDir.detailFile(partes[1]);
-            new Main().cat(file);
+            tree(currentDir);
             break;
           case "rename":
             Item i = currentDir.getChild(partes[1]);
-            if(i != null){
-              new Main().rename(i, partes[2]);
-            }else{
-              System.out.println("Erro: Arquivo ou diretório não existe");
+            rename(i, partes[2]);
+            break;
+          case "ls":
+            ls(currentDir);
+            break;
+          case "touch":
+            if(partes.length == 2) {
+              File f = touch(partes[1], "");
+              currentDir.addFile(f);
+              break;
             }
+            File f = touch(partes[1], partes[2]);
+            currentDir.addFile(f);
+            break;
+          case "echo":
+            int nPartes = partes.length;
+
+            String conteudo = "";
+            for(int i2 = 1; i2 < nPartes -2; i2++) {
+              conteudo += partes[i2] + " ";
+            }
+            
+            boolean substituir = partes[nPartes -2].equals(">");
+            String nomeArquivo = partes[nPartes -1];
+            Item item = currentDir.getChild(nomeArquivo);
+            if(item == null) {
+              item = new File(nomeArquivo, "");
+            }
+            if(item.getType().equals("dir")) {
+              throw new Exception("Erro: Não é possível escrever em um diretório");
+            }
+
+            try {
+              if(substituir) {
+                ((File) item).setContent(conteudo.trim());
+              } else {
+                ((File) item).appendContent(conteudo.trim());
+              }
+            } catch (Exception e) {
+              item = new File(nomeArquivo, conteudo.trim());
+              currentDir.addFile((File) item);
+            }
+            break;
+          case "cat":
+            File file = currentDir.detailFile(partes[1]);
+            cat(file);
+            break;
+          case "rm":
+            currentDir.removeFile(partes[1]);
             break;
           case "exit":
             System.out.println("Saindo...");
